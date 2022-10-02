584351e00bc85ebfa657d8bada19bb0736071cfc
Krista Cannady <kristalcannady@gmail.com>
2022-10-02 13:17:39 -0400

    java updates

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,13 @@
-// API URL = 'https://api.themoviedb.org/3/movie/550?api_key=96b0ba6aa451faff19f357f4cbce740b';
+// Setting up the search button variable
+var searchBtn = document.getElementById("clickme");
+// API URL with API Key.. I had to make an account to get/use this link 
+var APIURL = "https://api.themoviedb.org/3/movie/550?api_key=96b0ba6aa451faff19f357f4cbce740b";
+
+
+// Shows that calling on the API works 
+fetch(APIURL)
+.then((response) => {
+    console.log("We got a response!");
+    console.log(response);
+});
+
