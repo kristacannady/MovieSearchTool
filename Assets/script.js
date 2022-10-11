1f00e1091b5bf1f436338c781ef50e39f76be914
Krista Cannady <kristalcannady@gmail.com>
2022-10-10 20:40:12 -0400

    api working

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,24 @@
 // API URL with API Key.. I had to make an account to get/use this link 
-// var APIURL = "https://api.themoviedb.org/3/movie/550?api_key=96b0ba6aa451faff19f357f4cbce740b";
+// var APIURL = 
+https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
 
-// Variables for search button
-let searchBtn = document.getElementById("search-btn");
-let movieInp = document.getElementById("movie-inp");
-let api_key = "96b0ba6aa451faff19f357f4cbce740b";
-var queryURL = "https://api.themoviedb.org/3/movie/${movie_inp}?api_key=${api_key}&language=en-US";
+// Variables for search button and API call 
+var searchBtn = document.getElementById("search-btn");
+var movieInp = document.getElementById("movie-inp");
+var api_key = "&api_key=96b0ba6aa451faff19f357f4cbce740b";
+var searchurl = `https://api.themoviedb.org/3/search/movie?query=${movieInp}${api_key}`;
+
+searchBtn.addEventListener("click", () => {
+  console.log("I have been clicked")
+});
+
+// function for current condition
+function MovieSearch(movieInp) {
+      $.ajax({
+          url: searchurl,
+          method: "GET"
+      }).then(function(cityWeatherResponse) {
+          console.log(cityWeatherResponse);
+          
+          $("#weatherContent").css("display", "block");
+          $("#cityDetail").empty();
