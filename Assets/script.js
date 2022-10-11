cf5272e361dac1d86138c0a18cb2d6bb749513d2
Krista Cannady <kristalcannady@gmail.com>
2022-10-10 22:05:34 -0400

    read me photo added

--- Assets/script.js
+++ Assets/script.js
@@ -6,20 +6,17 @@ var searchBtn = document.getElementById("search-btn");
 const movieInp = document.getElementById("movie-inp");
 // Gets the input from the search the user put in
 const moviesearch = movieInp.value;
-const api_key = "&api_key=96b0ba6aa451faff19f357f4cbce740b";
-const searchurl = "https://api.themoviedb.org/3/search/movie?api_key=96b0ba6aa451faff19f357f4cbce740b";
-// Makes the URL dynamic so that the search can be used
-const APIURL = searchurl + '&query=' + moviesearch;
+const api_key = "96b0ba6aa451faff19f357f4cbce740b";
+const URL = 'https://api.themoviedb.org/3/search/movie?api_key=96b0ba6aa451faff19f357f4cbce740b&language=en-US&query=${movieInp.value}';
 
 // Listens for when search button is clicked
 searchBtn.addEventListener("click", fetchData);
-
 
 // Function for getting the data from the API
 function fetchData() {
   // This shows that the input is being read when the search button is clicked
   console.log(movieInp.value); 
-  fetch(APIURL)
+  fetch(URL)
   .then((response) => response.json())
   .then((data) => console.log(data))
 };
--- README.md
+++ README.md
@@ -1,20 +1,16 @@
 # Movie Search Tool 
 
 ## Purpose
-This interactive application allows you to search for movies from a masssive database. When you enter your search words and press 'search,' your results are displayed. 
+This interactive application allows you to search for movies from a massive database. This used the API from The Movie DB. When you enter your search words and press 'search,' your results are displayed.
 
-<!-- Need to update these photos  -->
-![alt text](./Assets/images/Screen%20Shot%202022-09-12%20at%208.48.06%20PM.png)
-![alt text](./Assets/images/Screen%20Shot%202022-09-12%20at%208.48.28%20PM.png)
+![alt text](./Assets/Images/SearchTool.png)
 ## Built With
 * HTML
 * CSS
 * CSS Framework: Bulma: https://bulma.io/ 
 * JavaScript
-<!-- Update these -->
+* API: The Movie DB 
 * API: 
-* API: 
-
 ## Website
 https://kristacannady.github.io/MovieSearchTool/ 
 ## Contribution
