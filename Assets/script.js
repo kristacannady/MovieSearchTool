0421feb6e2724d094473adc023030c701f61cdce
Krista Cannady <kristalcannady@gmail.com>
2022-10-10 21:20:30 -0400

    api working

--- Assets/script.js
+++ Assets/script.js
@@ -4,8 +4,8 @@ https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
 // Variables for search button and API call 
 var searchBtn = document.getElementById("search-btn");
 var movieInp = document.getElementById("movie-inp");
-var api_key = "&api_key=96b0ba6aa451faff19f357f4cbce740b";
-var searchurl = `https://api.themoviedb.org/3/search/movie?query=${search}${api_key}`;
+const api_key = "&api_key=96b0ba6aa451faff19f357f4cbce740b";
+const searchurl = "https://api.themoviedb.org/3/search/movie?api_key=96b0ba6aa451faff19f357f4cbce740b&language=en-US&query=String&page=1&include_adult=false";
 
 // Listens for when search button is clicked 
 searchBtn.addEventListener("click", fetchData);
