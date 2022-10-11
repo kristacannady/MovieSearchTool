c5fa1c126edd3f1e9fbfa069b5d9f4087e2a9a48
Krista Cannady <kristalcannady@gmail.com>
2022-10-10 21:40:23 -0400

    movie input is being read

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,25 @@
 // API URL with API Key.. I had to make an account to get/use this link
 https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
 
-// Variables for search button and API call 
+// Variables for search button and API call
 var searchBtn = document.getElementById("search-btn");
-var movieInp = document.getElementById("movie-inp");
+const movieInp = document.getElementById("movie-inp");
+// Gets the input from the search the user put in
+const moviesearch = movieInp.value;
 const api_key = "&api_key=96b0ba6aa451faff19f357f4cbce740b";
-const searchurl = "https://api.themoviedb.org/3/search/movie?api_key=96b0ba6aa451faff19f357f4cbce740b&language=en-US&query=String&page=1&include_adult=false";
+const searchurl = "https://api.themoviedb.org/3/search/movie?api_key=96b0ba6aa451faff19f357f4cbce740b";
+// Makes the URL dynamic so that the search can be used
+const APIURL = searchurl + '&query=' + moviesearch;
 
-// Listens for when search button is clicked 
+// Listens for when search button is clicked
 searchBtn.addEventListener("click", fetchData);
 
-// Function for getting the data from the API 
+
+// Function for getting the data from the API
 function fetchData() {
-  fetch(searchurl)
+  // This shows that the input is being read when the search button is clicked
+  console.log(movieInp.value); 
+  fetch(APIURL)
   .then((response) => response.json())
   .then((data) => console.log(data))
 };
