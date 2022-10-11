fdcc07ab44cbd4755fc35aa69d5304a66e80a248
Krista Cannady <kristalcannady@gmail.com>
2022-10-11 10:33:42 -0400

    second api

--- Assets/script.js
+++ Assets/script.js
@@ -26,9 +26,12 @@ result.innerHTML = `
         </div>
     </div>
     `;
+    return result;
   }
 
-// Function for getting the data from the API
+
+
+// Function for getting the data from the API WORKING!! :) 
 function fetchData(movie) {
   var searchURL = `https://api.themoviedb.org/3/search/movie?api_key=96b0ba6aa451faff19f357f4cbce740b&language=en-US&query=${movieInp.value}`;
   // This shows that the input is being read when the search button is clicked
--- README.md
+++ README.md
@@ -9,8 +9,8 @@ This interactive application allows you to search for movies from a massive data
 * CSS
 * CSS Framework: Bulma: https://bulma.io/ 
 * JavaScript
-* API: The Movie DB 
-* API: 
+* API: TMBD Movie Search API https://developers.themoviedb.org/3/search/search-movies
+* API: TMBD Image API https://developers.themoviedb.org/3/getting-started/images
 ## Website
 https://kristacannady.github.io/MovieSearchTool/ 
 ## Contribution
